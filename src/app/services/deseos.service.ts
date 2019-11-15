d1494faca95ed34f681471f13a8d897223b8fc0b
Rebeca Aunon <raunon@futurespace.es>
2019-11-15 15:47:40 +0100

    lista de items

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,10 +9,41 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
+    this.cargarStorage();
+    /*
     const lista1 = new Lista ( 'Recolectar piedras del infinito' );
     const lista2 = new Lista ( 'Héroes a desaparecer' );
     this.listas.push(lista1, lista2);
+
     console.log(this.listas);
+        */
   }
+
+  crearLista( titulo: string ) {
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+    return nuevaLista.id;
+  }
+
+  obtenerLista( id: string | number ) {
+    id = Number(id);
+    return this.listas.find( listaData => {
+      return listaData.id === id;
+    });
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify( this.listas ) );
+  }
+
+  cargarStorage() {
+    if (localStorage.getItem('data')) {
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
+  }
+
 
 }
